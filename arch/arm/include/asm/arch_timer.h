124481f84798c7c9f845092228814da36eaf5385
Will Deacon <will.deacon@arm.com>
2012-07-06 15:46:45 +0100

    ARM: 7451/1: arch timer: implement read_current_timer and get_cycles
    This patch implements read_current_timer using the architected timers
    when they are selected via CONFIG_ARM_ARCH_TIMER. If they are detected
    not to be usable at runtime, we return -ENXIO to the caller.
    
    Furthermore, if read_current_timer is exported then we can implement
    get_cycles in terms of it for use as both an entropy source and for
    implementing __udelay and friends.
    
    Tested-by: Shinya Kuribayashi <shinya.kuribayashi.px@renesas.com>
    Reviewed-by: Stephen Boyd <sboyd@codeaurora.org>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    
    Conflicts:
    
    	arch/arm/include/asm/arch_timer.h
    
    Change-Id: Ifddaf2bd958f0d0fd92a846575430a276edc4a42

--- arch/arm/include/asm/arch_timer.h
+++ arch/arm/include/asm/arch_timer.h
@@ -3,12 +3,14 @@
 
 #include <linux/ioport.h>
 #include <linux/clocksource.h>
+#include <asm/errno.h>
 
 struct arch_timer {
 	struct resource	res[2];
 };
 
 #ifdef CONFIG_ARM_ARCH_TIMER
+#define ARCH_HAS_READ_CURRENT_TIMER
 int arch_timer_of_register(void);
 int arch_timer_sched_clock_init(void);
 
