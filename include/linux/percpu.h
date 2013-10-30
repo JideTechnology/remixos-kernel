e081b294d5c876a414082843b4569b0f2700ea50
Greg Thelen <gthelen@google.com>
2013-10-30 13:56:20 -0700

    percpu: fix this_cpu_sub() subtrahend casting for unsigneds
    this_cpu_sub() is implemented as negation and addition.
    
    This patch casts the adjustment to the counter type before negation to
    sign extend the adjustment.  This helps in cases where the counter type
    is wider than an unsigned adjustment.  An alternative to this patch is
    to declare such operations unsupported, but it seemed useful to avoid
    surprises.
    
    This patch specifically helps the following example:
      unsigned int delta = 1
      preempt_disable()
      this_cpu_write(long_counter, 0)
      this_cpu_sub(long_counter, delta)
      preempt_enable()
    
    Before this change long_counter on a 64 bit machine ends with value
    0xffffffff, rather than 0xffffffffffffffff.  This is because
    this_cpu_sub(pcp, delta) boils down to this_cpu_add(pcp, -delta),
    which is basically:
      long_counter = 0 + 0xffffffff
    
    Also apply the same cast to:
      __this_cpu_sub()
      __this_cpu_sub_return()
      this_cpu_sub_return()
    
    All percpu_test.ko passes, especially the following cases which
    previously failed:
    
      l -= ui_one;
      __this_cpu_sub(long_counter, ui_one);
      CHECK(l, long_counter, -1);
    
      l -= ui_one;
      this_cpu_sub(long_counter, ui_one);
      CHECK(l, long_counter, -1);
      CHECK(l, long_counter, 0xffffffffffffffff);
    
      ul -= ui_one;
      __this_cpu_sub(ulong_counter, ui_one);
      CHECK(ul, ulong_counter, -1);
      CHECK(ul, ulong_counter, 0xffffffffffffffff);
    
      ul = this_cpu_sub_return(ulong_counter, ui_one);
      CHECK(ul, ulong_counter, 2);
    
      ul = __this_cpu_sub_return(ulong_counter, ui_one);
      CHECK(ul, ulong_counter, 1);
    
    Signed-off-by: Greg Thelen <gthelen@google.com>
    Acked-by: Tejun Heo <tj@kernel.org>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/percpu.h
+++ include/linux/percpu.h
@@ -332,7 +332,7 @@ do {									\
 #endif
 
 #ifndef this_cpu_sub
-# define this_cpu_sub(pcp, val)		this_cpu_add((pcp), -(val))
+# define this_cpu_sub(pcp, val)		this_cpu_add((pcp), -(typeof(pcp))(val))
 #endif
 
 #ifndef this_cpu_inc
@@ -402,7 +402,7 @@ do {									\
 # define this_cpu_add_return(pcp, val)	__pcpu_size_call_return2(this_cpu_add_return_, pcp, val)
 #endif
 
-#define this_cpu_sub_return(pcp, val)	this_cpu_add_return(pcp, -(val))
+#define this_cpu_sub_return(pcp, val)	this_cpu_add_return(pcp, -(typeof(pcp))(val))
 #define this_cpu_inc_return(pcp)	this_cpu_add_return(pcp, 1)
 #define this_cpu_dec_return(pcp)	this_cpu_add_return(pcp, -1)
 
@@ -570,7 +570,7 @@ do {									\
 #endif
 
 #ifndef __this_cpu_sub
-# define __this_cpu_sub(pcp, val)	__this_cpu_add((pcp), -(val))
+# define __this_cpu_sub(pcp, val)	__this_cpu_add((pcp), -(typeof(pcp))(val))
 #endif
 
 #ifndef __this_cpu_inc
@@ -636,7 +636,7 @@ do {									\
 	__pcpu_size_call_return2(__this_cpu_add_return_, pcp, val)
 #endif
 
-#define __this_cpu_sub_return(pcp, val)	__this_cpu_add_return(pcp, -(val))
+#define __this_cpu_sub_return(pcp, val)	__this_cpu_add_return(pcp, -(typeof(pcp))(val))
 #define __this_cpu_inc_return(pcp)	__this_cpu_add_return(pcp, 1)
 #define __this_cpu_dec_return(pcp)	__this_cpu_add_return(pcp, -1)
 
