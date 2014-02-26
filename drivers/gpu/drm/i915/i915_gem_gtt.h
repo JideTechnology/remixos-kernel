d3521b6bf0cc94d0b30e513331998bc8595c7d7f
Ben Widawsky <benjamin.widawsky@intel.com>
2014-02-26 12:14:47 -0800

    FROM_UPSTREAM [VPG]: drm/i915/gen8: Un-hardcode number of page directories
    Signed-off-by: Ben Widawsky <ben@bwidawsk.net>
    Signed-off-by: Michel Thierry <michel.thierry@intel.com>
    
    This is a backport of v4 of "PPGTT dynamic page allocations" posted in
    intel-gfx.
    
    Change-Id: Ie789048eb28e66941c5ab49bbf689332bd556c21
    Upstream-repo: https://www.mail-archive.com/intel-gfx@lists.freedesktop.org/msg51902.html
    Upstream-commit: none
    Upstream-as-of-tag: none
    For: GMINL-5449
    For: VIZ-4619
    Signed-off-by: Michel Thierry <michel.thierry@intel.com>

--- drivers/gpu/drm/i915/i915_gem_gtt.h
+++ drivers/gpu/drm/i915/i915_gem_gtt.h
@@ -261,7 +261,7 @@ struct i915_hw_ppgtt {
 	};
 	union {
 		dma_addr_t *pt_dma_addr;
-		dma_addr_t *gen8_pt_dma_addr[4];
+		dma_addr_t *gen8_pt_dma_addr[GEN8_LEGACY_PDPES];
 	};
 
 	struct list_head vma_list;
