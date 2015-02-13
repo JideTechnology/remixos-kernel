081fbab255d8b1e45aace34ee508378aa6181b38
liming <liming@allwinnertech.com>
2015-02-13 17:07:58 +0800

    PMU:pmu load when has received notifier.
            pmu driver load,when it has received cpus,s notifier.
    
    Signed-off-by: liming <liming@allwinnertech.com>

--- drivers/power/axp_power/axp81x/axp81x-board-device.c
+++ drivers/power/axp_power/axp81x/axp81x-board-device.c
@@ -14,25 +14,42 @@
 #include <linux/platform_device.h>
 #include <linux/err.h>
 
-#ifdef	CONFIG_AXP_TWI_USED
-#else
+#ifdef CONFIG_SUNXI_ARISC
+#include <linux/arisc/arisc-notifier.h>
+static struct notifier_block axp81x_nb;
+
 static struct platform_device axp81x_platform_device = {
 	.name		    = "axp81x_board",
 	.id		        = PLATFORM_DEVID_NONE,
 };
+
+static int axp81x_board_device_event(struct notifier_block *nb, unsigned long event,
+	 void *data)
+{
+	s32 ret = 0;
+
+	if (ARISC_INIT_READY == event) {
+		ret = platform_device_register(&axp81x_platform_device);
+		if (IS_ERR_VALUE(ret)) {
+				printk("register axp81x platform device failed\n");
+				return ret;
+		}
+
+	}
+	return 0;
+}
 #endif
 
 static s32 __init axp81x_board_device_init(void)
 {
 	s32 ret = 0;
 
-#ifdef	CONFIG_AXP_TWI_USED
+#ifdef CONFIG_AXP_TWI_USED
 #else
-	ret = platform_device_register(&axp81x_platform_device);
-	if (IS_ERR_VALUE(ret)) {
-			printk("register axp81x platform device failed\n");
-			return ret;
-	}
+#ifdef CONFIG_SUNXI_ARISC
+	axp81x_nb.notifier_call = axp81x_board_device_event;
+	ret = arisc_register_notifier(&axp81x_nb);
+#endif
 #endif
         return ret;
 }
