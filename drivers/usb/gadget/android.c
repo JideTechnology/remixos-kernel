e86d885d684bacc1a47fe9ac084f4e0b49f548f9
wangjx <wangjx@allwinnertech.com>
2015-02-13 17:03:56 +0800

    usb: adb: sunxi use adb instead of ffs
    Signed-off-by: wangjx <wangjx@allwinnertech.com>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -36,6 +36,9 @@
 #include "f_audio_source.c"
 #endif
 #include "f_mass_storage.c"
+#ifdef CONFIG_USB_SUNXI_USB_ADB
+#include "f_adb.c"
+#endif
 #include "f_mtp.c"
 #include "f_accessory.c"
 #ifdef CONFIG_NET
@@ -446,7 +449,99 @@ static void *functionfs_acquire_dev_callback(const char *dev_name)
 static void functionfs_release_dev_callback(struct ffs_data *ffs_data)
 {
 }
+#ifdef CONFIG_USB_SUNXI_USB_ADB
+struct adb_data {
+	bool opened;
+	bool enabled;
+};
 
+static int
+adb_function_init(struct android_usb_function *f,
+		struct usb_composite_dev *cdev)
+{
+	f->config = kzalloc(sizeof(struct adb_data), GFP_KERNEL);
+	if (!f->config)
+		return -ENOMEM;
+
+	return adb_setup();
+}
+
+static void adb_function_cleanup(struct android_usb_function *f)
+{
+	adb_cleanup();
+	kfree(f->config);
+}
+
+static int
+adb_function_bind_config(struct android_usb_function *f,
+		struct usb_configuration *c)
+{
+	return adb_bind_config(c);
+}
+
+static void adb_android_function_enable(struct android_usb_function *f)
+{
+	struct android_dev *dev = _android_dev;
+	struct adb_data *data = f->config;
+
+	data->enabled = true;
+
+	/* Disable the gadget until adbd is ready */
+	if (!data->opened)
+		android_disable(dev);
+}
+
+static void adb_android_function_disable(struct android_usb_function *f)
+{
+	struct android_dev *dev = _android_dev;
+	struct adb_data *data = f->config;
+
+	data->enabled = false;
+
+	/* Balance the disable that was called in closed_callback */
+	if (!data->opened)
+		android_enable(dev);
+}
+
+static struct android_usb_function adb_function = {
+	.name		= "adb",
+	.enable		= adb_android_function_enable,
+	.disable	= adb_android_function_disable,
+	.init		= adb_function_init,
+	.cleanup	= adb_function_cleanup,
+	.bind_config	= adb_function_bind_config,
+};
+
+static void adb_ready_callback(void)
+{
+	struct android_dev *dev = _android_dev;
+	struct adb_data *data = adb_function.config;
+
+	mutex_lock(&dev->mutex);
+
+	data->opened = true;
+
+	if (data->enabled)
+		android_enable(dev);
+
+	mutex_unlock(&dev->mutex);
+}
+
+static void adb_closed_callback(void)
+{
+	struct android_dev *dev = _android_dev;
+	struct adb_data *data = adb_function.config;
+
+	mutex_lock(&dev->mutex);
+
+	data->opened = false;
+
+	if (data->enabled)
+		android_disable(dev);
+
+	mutex_unlock(&dev->mutex);
+}
+#endif
 #define MAX_ACM_INSTANCES 4
 struct acm_function_config {
 	int instances;
@@ -1067,6 +1162,9 @@ static struct android_usb_function audio_source_function = {
 
 static struct android_usb_function *supported_functions[] = {
 	&ffs_function,
+#ifdef CONFIG_USB_SUNXI_USB_ADB
+	&adb_function,
+#endif
 	&acm_function,
 	&mtp_function,
 	&ptp_function,
