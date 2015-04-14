203ccfee1e53d580e9dc992fe6a94d660e2865fe
zengshuchuan <zengshuchuan@allwinnertech.com>
2015-04-14 09:41:18 +0800

    input: dont send power key when resume
    we can not long press power key when suspend

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -671,6 +671,8 @@ static void input_dev_release_keys(struct input_dev *dev)
 
 	if (is_event_supported(EV_KEY, dev->evbit, EV_MAX)) {
 		for (code = 0; code <= KEY_MAX; code++) {
+			if (code == KEY_POWER)
+				continue;
 			if (is_event_supported(code, dev->keybit, KEY_MAX) &&
 			    __test_and_clear_bit(code, dev->key)) {
 				input_pass_event(dev, EV_KEY, code, 0);
