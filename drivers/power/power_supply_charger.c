f79cebc3a98ae5f92103d0e480d79552e05ad9d1
Gerard Andre <gerard.andre@intel.com>
2015-04-14 11:45:29 +0200

    Check get_bat_prop_cache in trigger_algo
    In trigger_algo, if get_bat_prop_cache does not
    find the property, trigger_algo uses the value
    found on its stack.
    
    Change-Id: I125095c221cf14790fb454cd8d0e31dc0edccc53
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-29853
    Signed-off-by: Jean-Christophe PINCE <jean-christophe.pince@intel.com>
    Reviewed-on: https://android.intel.com:443/351787

--- drivers/power/power_supply_charger.c
+++ drivers/power/power_supply_charger.c
@@ -707,7 +707,7 @@ static int trigger_algo(struct power_supply *psy)
 	struct batt_props bat_prop;
 	struct charging_algo *algo;
 	struct ps_batt_chg_prof chrg_profile;
-	int cnt;
+	int cnt, ret;
 
 	if (psy->type != POWER_SUPPLY_TYPE_BATTERY)
 		return 0;
@@ -718,7 +718,12 @@ static int trigger_algo(struct power_supply *psy)
 	}
 
 
-	get_bat_prop_cache(psy, &bat_prop);
+	ret = get_bat_prop_cache(psy, &bat_prop);
+	if (ret) {
+		pr_err("%s:Error in getting the battery property of %s!!\n",
+							__func__, psy->name);
+		return ret;
+	}
 
 	algo = power_supply_get_charging_algo(psy, &chrg_profile);
 	if (!algo) {
