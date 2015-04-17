9a29bb1af6c9b94444f0af70bb747a735b7b06e2
Per Persson <per.persson@intel.com>
2015-04-17 11:16:36 +0200

    Revert "atomisp2: Revise zoom factor configuration"
    This reverts commit 192d3c0e241f70609d21baa1abcea0fd231da31a.
    
    Change-Id: I7b584dadb4e47b4910f0d243d81d3a7b32e5b68a

--- drivers/external_drivers/camera/drivers/media/pci/atomisp2/atomisp_driver/atomisp_compat_css20.c
+++ drivers/external_drivers/camera/drivers/media/pci/atomisp2/atomisp_driver/atomisp_compat_css20.c
@@ -3793,20 +3793,19 @@ void atomisp_css_set_zoom_factor(struct atomisp_sub_device *asd,
 {
 	struct atomisp_device *isp = asd->isp;
 
-	if (zoom == asd->params.css_param.dz_config.dx &&
-		 zoom == asd->params.css_param.dz_config.dy) {
+	if (!asd->params.config.dz_config)
+		asd->params.config.dz_config = &asd->params.dz_config;
+
+	if (zoom == asd->params.config.dz_config->dx &&
+		 zoom == asd->params.config.dz_config->dy) {
 		dev_dbg(isp->dev, "same zoom scale. skipped.\n");
 		return;
 	}
 
-	memset(&asd->params.css_param.dz_config, 0,
+	memset(asd->params.config.dz_config, 0,
 		sizeof(struct ia_css_dz_config));
-	asd->params.css_param.dz_config.dx = zoom;
-	asd->params.css_param.dz_config.dy = zoom;
-
-	asd->params.css_param.update_flag.dz_config =
-		(struct atomisp_dz_config *) &asd->params.css_param.dz_config;
-	asd->params.css_update_params_needed = true;
+	asd->params.dz_config.dx = zoom;
+	asd->params.dz_config.dy = zoom;
 }
 
 void atomisp_css_set_formats_config(struct atomisp_sub_device *asd,
