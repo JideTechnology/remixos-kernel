184e67aea5fe0d9d5e4bfd3ff76848d0454b8e2b
Wu, Hao <hao.wu@intel.com>
2015-05-24 11:02:37 +0800

    Revert "REVERTME: xhci-intel-cap: don't poll CFG1 when switch to device mode"
    This reverts commit 1721155fb7cc89841c97e32d7e1dd8260cb70667.
    
    Change-Id: I5ad71a3c20d56f72a87b6d54bb3c7b8e72024a69
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-35534
    Signed-off-by: Wu, Hao <hao.wu@intel.com>

--- drivers/usb/host/xhci-intel-cap.c
+++ drivers/usb/host/xhci-intel-cap.c
@@ -117,9 +117,8 @@ int xhci_intel_phy_mux_switch(struct xhci_hcd *xhci, int is_device_on)
 	while (!time_after(jiffies, timeout)) {
 		data = readl(xhci->phy_mux_regs + DUAL_ROLE_CFG1);
 		if (is_device_on) {
-			/* HACK: don't poll CFG1 when swtich to device
-			   mode, as it is always timed out. */
-			break;
+			if (!(data & SW_MODE))
+				break;
 		} else {
 			if (data & SW_MODE)
 				break;
