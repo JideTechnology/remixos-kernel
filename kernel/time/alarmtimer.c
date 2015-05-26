3435a3bbe515431cd1cb3bd1d3921252b4785f69
Simon Dubray <simonx.dubray@intel.com>
2015-05-26 10:52:40 +0200

    alarmtimer: adjust the duration of __pm_wakeup_event
    When an alarm expires in the two next seconds, alarmtimer aborts the suspend
    and uses a wakeup event to prevent suspend during two seconds.
    Because of this, we are seing big wakeups (about 3 seconds instead of 1).
    
    Change-Id: I2f0890ffcfe3ad6f4ce4544a4d78f0796ed844a7
    Signed-off-by: Simon Dubray <simonx.dubray@intel.com>
    Signed-off-by: Charly Dumur <charly.dumur@intel.com>

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -255,7 +255,7 @@ static int alarmtimer_suspend(struct device *dev)
 		return 0;
 
 	if (ktime_to_ns(min) < 2 * NSEC_PER_SEC) {
-		__pm_wakeup_event(ws, 2 * MSEC_PER_SEC);
+		__pm_wakeup_event(ws, ktime_to_ms(min) + 1);
 		return -EBUSY;
 	}
 
