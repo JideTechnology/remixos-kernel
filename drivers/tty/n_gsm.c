9bda88ec9c90dfd046575f6d96b786589fbbd946
xinhui.pan <xinhuix.pan@intel.com>
2015-05-27 14:09:53 +0800

    tty/n_gsm.c: fix a memory leak when gsmtty is removed
    When gsmtty_remove put dlci, it will cause memory leak
    if dlci->port's refcount is zero. So we do the cleanup
    work in .cleanup callback instead.
    
    dlci will be last put in two call chains.
    1) gsmld_close -> gsm_cleanup_mux -> gsm_dlci_release ->dlci_put
    2) gsmld_remove -> dlci_put
    so there is a race. the memory leak depends on the race.
    
    In call chain 2. we hit the memory leak. below comment tells.
    
    release_tty -> tty_driver_remove_tty -> gsmtty_remove ->dlci_put
    -> tty_port_destructor (WARN_ON(port->itty) and return directly)
                             |
                    tty->port->itty = NULL;
                             |
                    tty_kref_put ---> release_one_tty
                    -> gsmtty_cleanup (added by our patch)
    
    So our patch fix the memory leak by doing the cleanup work
    after tty core did.
    
    Change-Id: I0feba67ce441e6e945d6423ee62a77a1af38b46e
    Tracked-On: https://jira01.devtools.intel.com/browse/AKP-1778
    Signed-off-by: Pan Xinhui <xinhuix.pan@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Reviewed-on: https://android.intel.com:443/372500

--- drivers/tty/n_gsm.c
+++ drivers/tty/n_gsm.c
@@ -3567,7 +3567,7 @@ static int gsmtty_break_ctl(struct tty_struct *tty, int state)
 	return gsmtty_modem_update(dlci, encode);
 }
 
-static void gsmtty_remove(struct tty_driver *driver, struct tty_struct *tty)
+static void gsmtty_cleanup(struct tty_struct *tty)
 {
 	struct gsm_dlci *dlci = tty->driver_data;
 	struct gsm_mux *gsm = dlci->gsm;
@@ -3579,7 +3579,6 @@ static void gsmtty_remove(struct tty_driver *driver, struct tty_struct *tty)
 	mux_put(gsm);
 	tty->driver_data = NULL;
 	tty->port = NULL;
-	driver->ttys[tty->index] = NULL;
 }
 
 /* Virtual ttys for the demux */
@@ -3604,7 +3603,7 @@ static const struct tty_operations gsmtty_ops = {
 	.tiocmget		= gsmtty_tiocmget,
 	.tiocmset		= gsmtty_tiocmset,
 	.break_ctl		= gsmtty_break_ctl,
-	.remove			= gsmtty_remove,
+	.cleanup		= gsmtty_cleanup,
 };
 
 
