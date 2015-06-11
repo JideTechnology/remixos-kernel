8cb6776977264da269a93a69b59e4829aad9d5f4
Dwane Pottratz <dwane.pottratz@intel.com>
2015-06-10 17:04:05 -0700

    Revert "Prevent ccsm to do c-type detection in reverse boost mode"
    This reverts commit 3f12e111a80e4c0a7bc42ba946341deed9d4799e.
    
    Change-Id: Ic7b83c5ea2ec3a0d1a2af08aa693579c1f8c96b7
    Signed-off-by: Mustapha Ben Zoubeir <mustapha.ben.zoubeir@intel.com>

--- drivers/power/intel_pmic_ccsm.h
+++ drivers/power/intel_pmic_ccsm.h
@@ -389,7 +389,6 @@ struct pmic_chrgr_drv_context {
 	int pmic_model;
 	int intmap_size;
 	int reg_cnt;
-	int cable_state;
 	void __iomem *pmic_intr_iomap;
 	struct pmic_regs *reg_map;
 	struct device *dev;
@@ -402,7 +401,4 @@ struct pmic_chrgr_drv_context {
 	struct thermal_cooling_device *vbus_cdev;
 	struct list_head evt_queue;
 	struct delayed_work evt_work;
-	struct extcon_specific_cable_nb host_cable;
-	struct notifier_block cable_nb;
-	struct work_struct extcon_work;
 };
