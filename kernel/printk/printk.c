5a665918657b6eef19836944b013f2c479c6148e
Mustapha Ben Zoubeir <mustapha.ben.zoubeir@intel.com>
2015-06-15 09:57:35 +0200

    Revert "[BACKPORT] Current printk implementation can not fully support that"
    This reverts commit 34cc178f6e5cbc52c296dca07c2658d947c3f7b1.
    
    Change-Id: Ia68c5b334fc80f91df09474dc0855fddd0a6a000
    Signed-off-by: Mustapha Ben Zoubeir <mustapha.ben.zoubeir@intel.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -1348,7 +1348,7 @@ static int console_trylock_for_printk(unsigned int cpu)
 {
 	int retval = 0, wake = 0;
 
-	if (!in_nmi() && console_trylock()) {
+	if (console_trylock()) {
 		retval = 1;
 
 		/*
@@ -1527,13 +1527,7 @@ asmlinkage int vprintk_emit(int facility, int level,
 	}
 
 	lockdep_off();
-	if (unlikely(in_nmi())) {
-		if (!raw_spin_trylock(&logbuf_lock))
-			goto out_restore_lockdep_irqs;
-	} else {
-		raw_spin_lock(&logbuf_lock);
-	}
-
+	raw_spin_lock(&logbuf_lock);
 	logbuf_cpu = this_cpu;
 
 	if (recursion_bug) {
@@ -1636,7 +1630,6 @@ asmlinkage int vprintk_emit(int facility, int level,
 	if (console_trylock_for_printk(this_cpu))
 		console_unlock();
 
-out_restore_lockdep_irqs:
 	lockdep_on();
 out_restore_irqs:
 	local_irq_restore(flags);
