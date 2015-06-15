6173b389d49fcc107e293447e58e6ec64c4810c9
Huiquan Zhong <huiquan.zhong@intel.com>
2015-06-15 19:20:27 +0800

    spi/pxa2xx: fix SPI read/write issue after back from S3
    Sometimes, while resume back from S3, SPI can't read/write register.
    Need to reset the SPI controller first.
    
    Change-Id: I908318502fe92b9d59efdde4a1d040c732cbe688
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-39334
    Signed-off-by: Huiquan Zhong <huiquan.zhong@intel.com>

--- drivers/spi/spi-pxa2xx.c
+++ drivers/spi/spi-pxa2xx.c
@@ -1378,8 +1378,8 @@ static int pxa2xx_spi_resume(struct device *dev)
 	if (!pm_runtime_suspended(dev))
 		clk_prepare_enable(ssp->clk);
 
-	/* Restore LPSS private register bits */
-	lpss_ssp_setup(drv_data);
+	/* Init LPSS private register bits */
+	lpss_ssp_init(drv_data);
 
 	/* Start the queue running */
 	status = spi_master_resume(drv_data->master);
