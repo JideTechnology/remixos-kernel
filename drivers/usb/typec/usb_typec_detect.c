cc0f5ab3aa5a12551e3bde966b6b9822077ec2b6
Biju Chakkachattil <bijux.chakkachattil@intel.com>
2015-06-19 12:44:03 +0530

    EM/Typec:Revert "Update pmic reg for enabling host-mode"
    This reverts commit f997680c27fd2c253756f45b7f5dc89078f6d8d0.
    
    This is the workaround to enable OTG. This is no more required
    as the BIOS 4.82 has included the required changes.
    
    Change-Id: Idac39da3fda55d2e1d0619bd2b9a919d88122c31
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-36254
    Signed-off-by: Biju Chakkachattil <bijux.chakkachattil@intel.com>
    Reviewed-on: https://android.intel.com:443/382411

--- drivers/usb/typec/usb_typec_detect.c
+++ drivers/usb/typec/usb_typec_detect.c
@@ -203,10 +203,6 @@ static void detect_dfp_work(struct work_struct *work)
 
 			atomic_notifier_call_chain(&detect->otg->notifier,
 				USB_EVENT_ID, NULL);
-			/* [WA] Since neccessary pmic-gpio isn't exposed
-			 * as of now, this is a w/a to write directly.
-			 */
-			intel_soc_pmic_writeb(0x6e2d, 0x31);
 
 			return;
 		} else if (CC_RA(cc1.v_rd) && CC_RA(cc2.v_rd)) {
@@ -435,12 +431,7 @@ static void update_phy_state(struct work_struct *work)
 			typec_enable_autocrc(detect->phy, false);
 		} else {
 			/* state = DFP; disable VBUS */
-			/* [WA] Since neccessary pmic-gpio isn't exposed
-			 * as of now, this is a w/a to write directly.
-			 */
-			intel_soc_pmic_writeb(0x6e2d, 0x30);
 			extcon_set_cable_state(detect->edev, "USB-Host", false);
-
 			reinit_completion(&detect->lock_ufp_complete);
 			mutex_lock(&detect->lock);
 			detect->state = DETECT_STATE_LOCK_UFP;
