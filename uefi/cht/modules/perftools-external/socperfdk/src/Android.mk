c82101e3181f5a7ad0e35fd6667b88913fce4dd0
Ranjan Dutta <ranjan.dutta@intel.com>
2015-06-22 06:30:29 -0700

    Adding kernel modules for WW25
    Signed-off-by: Ranjan Dutta <ranjan.dutta@intel.com>

---
+++ uefi/cht/modules/perftools-external/socperfdk/src/Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := socperf1_2
+LOCAL_MODULE_PATH := $(LOCAL_PATH)
+LOCAL_MODULE_TAGS := debug
+include $(BUILD_EXTERNAL_KERNEL_MODULE)
+
