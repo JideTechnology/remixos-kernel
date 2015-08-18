dcb5ce1b66470b61d1177eac5fe89cf2c72780ad
b913 <b913@borqs.com>
2015-08-18 12:00:01 +0800

    Modify the sampling frequency of the gyroscope
    Change-Id: I32f92bb816b7fdb75d0ffcfae9894890e24f333c
    Borig-Change-Id: I51327e7a06b7a244856f5428ccb515d2e4743a6b
    Reviewed-on: http://192.168.1.79/gerrit/13150
    Reviewed-by: b199 <b199@borqs.com>

--- drivers/input/misc/BMG160/bmg160_driver.c
+++ drivers/input/misc/BMG160/bmg160_driver.c
@@ -61,7 +61,9 @@
 #define BMG_MAX_RETRY_WAIT_DRDY (100)
 
 #define BMG_DELAY_MIN (1)
-#define BMG_DELAY_DEFAULT (200)
+/* #define BMG_DELAY_DEFAULT (200) */
+#define BMG_DELAY_DEFAULT (20)
+
 
 #define BMG_VALUE_MAX (32767)
 #define BMG_VALUE_MIN (-32768)
