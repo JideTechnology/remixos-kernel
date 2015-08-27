2d238991a6ef968f409fd0561fc9eb282e71c8e1
liming <liming@allwinnertech.com>
2015-08-27 14:42:18 +0800

    clk:modify clk bind module error
        modify clk bind module error.
    
    Signed-off-by: liming <liming@allwinnertech.com>

--- include/linux/sunxi-clk-prepare.h
+++ include/linux/sunxi-clk-prepare.h
@@ -5,7 +5,7 @@
 extern int sunxi_clk_enable_prepare(struct clk *clk);
 extern int sunxi_clk_disable_prepare(struct clk *clk);
 #else
-int sunxi_clk_enable_prepare(struct clk *clk){return 0;};
-int sunxi_clk_disable_prepare(struct clk *clk){ return 0; };
+static int sunxi_clk_enable_prepare(struct clk *clk){return 0;};
+static int sunxi_clk_disable_prepare(struct clk *clk){ return 0; };
 #endif /* CONFIG_SUNXI_CLK_PREPARE */
 #endif /* _SUNXI_LINUX_CLK_PERPARE_H */
