13f8251774da416c0259f65232ac9e79d2e9d90f
Wiebe, Wladislav (Nokia - DE/Ulm) <wladislav.wiebe@nokia.com>
2015-11-13 12:10:05 +0000

    pstore: Add support for 64 Bit address space
    Some architectures have their reserved RAM in 64 Bit address space.
    Therefore convert mem_address module parameter to ullong.
    
    Signed-off-by: Wladislav Wiebe <wladislav.wiebe@nokia.com>
    Acked-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- include/linux/pstore_ram.h
+++ include/linux/pstore_ram.h
@@ -78,7 +78,7 @@ void ramoops_console_write_buf(const char *buf, size_t size);
 
 struct ramoops_platform_data {
 	unsigned long	mem_size;
-	unsigned long	mem_address;
+	phys_addr_t	mem_address;
 	unsigned int	mem_type;
 	unsigned long	record_size;
 	unsigned long	console_size;
