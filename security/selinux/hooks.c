87a26316423061b58b658ab4fc112538968880b2
Ma Jian <majian@jidemail.com>
2015-11-17 16:27:28 +0800

    kernel support selinux
    ref T6517
    tested: local build and run with usbdisk in pc
    Change-Id: Id5ef0af4a4a40f6905893aeb76689aa9df661253

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -457,8 +457,9 @@ static int sb_finish_set_opts(struct super_block *sb)
 	 * Special handling for rootfs. Is genfs but supports
 	 * setting SELinux context on in-core inodes.
 	 */
-	if (strncmp(sb->s_type->name, "rootfs", sizeof("rootfs")) == 0)
-		sbsec->flags |= SE_SBLABELSUPP;
+     // @jide
+	//if (strncmp(sb->s_type->name, "rootfs", sizeof("rootfs")) == 0)
+		//sbsec->flags |= SE_SBLABELSUPP;
 
 	/* Initialize the root inode. */
 	rc = inode_doinit_with_dentry(root_inode, root);
