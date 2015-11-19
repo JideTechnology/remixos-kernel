4aa3b9c4f6aaa9869d41c24923d53676055973c7
b928 <b928@borqs.com>
2015-11-19 10:57:14 +0800

    integrate LPM and AGNSS bring up patches
    Change-Id: I5ff75968cefc67054287723c25431dbad9ad7364
    Borig-Change-Id: I71d52cf3733aa4cba93c298aadb3720a0ccbe6d2
    Reviewed-on: http://192.168.1.79/gerrit/14974
    Reviewed-by: b571 <b571@borqs.com>
    Reviewed-on: https://android.intel.com:443/440643

--- drivers/tty/serial/hsu/hsu.h
+++ drivers/tty/serial/hsu/hsu.h
@@ -59,12 +59,12 @@ enum hsu_wake_src {
 struct uart_hsu_port;
 
 struct hsu_port_pin_cfg {
-	int wake_gpio;
-	int wake_src;
-	int rx_gpio;
-	int tx_gpio;
-	int rts_gpio;
-	int cts_gpio;
+	struct gpio_desc *wake_gpio;
+	enum hsu_wake_src wake_src;
+	struct gpio_desc *rx_gpio;
+	struct gpio_desc *tx_gpio;
+	struct gpio_desc *rts_gpio;
+	struct gpio_desc *cts_gpio;
 };
 
 struct hsu_port_cfg {
@@ -238,7 +238,6 @@ struct uart_hsu_port {
 	void			*dma_priv;
 	struct hsu_dma_ops	*dma_ops;
 	int			dma_inited;
-	int			prev_mcr;
 	struct hsu_port_cfg	*port_cfg;
 };
 
@@ -263,13 +262,9 @@ static inline unsigned int serial_in(struct uart_hsu_port *up, int offset)
 {
 	unsigned int val;
 
-	if (offset > UART_MSR || up->hw_type == hsu_dw) {
+	if (offset > UART_MSR || up->hw_type == hsu_dw)
 		val = readl(up->port.membase + (offset << 2));
-		if (offset == UART_MSR && up->prev_mcr & UART_MCR_AFE) {
-			val |= UART_MSR_CTS;
-			val &= ~UART_MSR_DCTS;
-		}
-	} else
+	else
 		val = (unsigned int)readb(up->port.membase + offset);
 
 	return val;
