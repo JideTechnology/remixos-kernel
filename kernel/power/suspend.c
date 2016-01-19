2fff68eb10f6d305ba03d4c557c3a13cced3c6ef
wenzengc <wenzeng.chen@intel.com>
2016-01-19 15:59:50 +0800

    pnp:fix the bug that sometimes system hang when wakeup by USB
    IPS: 6000150606
    
    the bug introduced by this commit
    7bd7a5c6a3fff5c8a34635d6eeefb5055ea1b571
    
    clockevents_notify(CLOCK_EVT_NOTIFY_RESUME, NULL) haven't called
    when syscore_suspend abort
    
    Change-Id: I2b0bdd545941620d7bfb440822bd050065a89db8
    Signed-off-by: wenzengc <wenzeng.chen@intel.com>
    Signed-off-by: hongliangw <hongliangx.wang@intel.com>
    Tracked-On:
    Reviewed-on: https://android.intel.com:443/460629

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -261,7 +261,8 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 			log_suspend_abort_reason(suspend_abort);
 		}
 		syscore_resume();
-	}
+	} else
+		clockevents_notify(CLOCK_EVT_NOTIFY_RESUME, NULL);
 
 	arch_suspend_enable_irqs();
 	BUG_ON(irqs_disabled());
